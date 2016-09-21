537c72442021a84588c39121c00c3a0bff0069b7
Seaim Khan <seaim.khan@dawsoncollege.qc.ca>
2016-09-21 17:24:49 -0400

    Initial commit

---
+++ app/src/main/java/seaimkhan/ca/countryapp/MainActivity.java
@@ -0,0 +1,25 @@
+package seaimkhan.ca.countryapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showMap(View v) {
+        Uri geoLocation = Uri.parse("geo:0,0?q="+ Uri.encode(getString(R.string.country)));
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+}
